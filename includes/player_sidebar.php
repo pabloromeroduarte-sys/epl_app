94a53ad7906ea9792378501e9de1b85701bf4305
Pablo Romero <pabloromeroduarte-sys@users.noreply.github.com>
2026-05-18 19:14:42 -0400

    Agregar Guias al menu inferior movil

--- includes/player_sidebar.php
+++ includes/player_sidebar.php
@@ -136,6 +136,11 @@ $_ranking = $_rk->fetch();
       <?php endif; ?>
       <span>Notif.</span>
     </a>
+    <a href="<?= epl_url('tutoriales.php') ?>" class="dash-bottom-link <?= $_ptab==='tutoriales'?'active':'' ?>">
+      <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/></svg>
+      <span>Guías</span>
+    </a>
+
     <a href="<?= epl_url('mi_perfil.php') ?>" class="dash-bottom-link <?= $_ptab==='perfil'?'active':'' ?>">
       <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
       <span>Perfil</span>
